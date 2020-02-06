8b90f51a9f196dc21ccb390bf1664508ebec135c
Ivan Tolkachev <vantrey@yandex.ru>
2020-02-06 11:14:37 +0300

    add tolkachev files

---
+++ src/Tolkachev.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Tolkachev.css'
+class Tolkachev extends React.Component {
+    render = () => {
+        return (
+                <div id="tolkachev">
+                    Толкачёв Иван Борисович
+                </div>
+        );
+    }
+}
+export default Tolkachev;
+
