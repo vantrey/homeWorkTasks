0560506c811713b2b0070db69ab5f6aab0ca006b
Ivan Tolkachev <vantrey@yandex.ru>
2020-02-07 09:16:39 +0300

    add tolkachev to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tolkachev from "./Tolkachev";
 
 function App() {
   return (
     <div className="App">
+      <Tolkachev/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
