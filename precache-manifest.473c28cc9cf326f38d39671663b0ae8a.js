b96134d48dc46b931f6b8b637da93a702e74ef93
Ivan Tolkachev <vantrey@yandex.ru>
2020-02-28 14:55:50 +0300

    Updates

---
+++ precache-manifest.473c28cc9cf326f38d39671663b0ae8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de3e5852ad91d9bfc94647f4057bde1",
+    "url": "/ignat-tasks/index.html"
+  },
+  {
+    "revision": "2e349bde427420b32710",
+    "url": "/ignat-tasks/static/css/main.80945b3b.chunk.css"
+  },
+  {
+    "revision": "b65e4967f1526fbe766a",
+    "url": "/ignat-tasks/static/js/2.cac5f1c1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ignat-tasks/static/js/2.cac5f1c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e349bde427420b32710",
+    "url": "/ignat-tasks/static/js/main.2cb86f37.chunk.js"
+  },
+  {
+    "revision": "d482873d6bd7c29361e6",
+    "url": "/ignat-tasks/static/js/runtime-main.cf1f753a.js"
+  }
+]);
